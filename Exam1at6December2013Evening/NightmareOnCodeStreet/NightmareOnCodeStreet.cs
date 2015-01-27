3601bebd2fc1875060651126574e34b75b469957
SimoPrG <SimoPrG@users.noreply.github.com>
2015-01-27 23:58:49 +0200

    2. NightmareOnCodeStreet

---
+++ Exam1at6December2013Evening/NightmareOnCodeStreet/NightmareOnCodeStreet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        long sum = 0;
+        long amount = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                if ((text[i] >= '0') && (text[i] <= '9'))
+                {
+                    amount++;
+                    sum += text[i] - '0';
+                }
+            }
+        }
+        Console.WriteLine("{0} {1}", amount, sum);
+    }
+}
