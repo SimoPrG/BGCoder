c26c1dcd3c460c97fb41e9d9c367b6f45f444635
SimoPrG <simeon_petev@hotmail.com>
2015-08-04 22:03:51 +0300

    Sample exam task 2 jQuery Tab Control

--- JavaScriptUIandDOM/SampleExam/task-2/task-files/scripts.js
+++ JavaScriptUIandDOM/SampleExam/task-2/task-files/scripts.js
@@ -1,3 +1,12 @@
 $.fn.tabs = function () {
+    var $currentTab;
+    this.addClass('tabs-container').on('click', '.tab-item', function () {
+        if ($currentTab) {
+            $currentTab.toggleClass('current').find('.tab-item-content').toggle();
+        }
 
+        $currentTab = $(this);
+        $currentTab.toggleClass('current').find('.tab-item-content').toggle();
+
+    }).find('.tab-item-content').hide();
 };
